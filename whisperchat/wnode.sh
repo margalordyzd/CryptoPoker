d7503e8d45193e003675dd6b2e79a70a476fbce6
margalordyzd <maldonadoraulqro@gmail.com>
2018-04-27 12:13:56 -0500

    creation of all

---
+++ whisperchat/wnode.sh
@@ -0,0 +1,13 @@
+> wnode -topic=a6fcb30d -asym -boot=enode://d25474361659861e9e651bc728a17e807a3359ca0d344afd544ed0f11a31faecaf4d74b55db53c6670fd624f08d5c79adfc8da5dd4a11b9213db49a3b750845e@52.178.209.125:30379
+
+
+my public key: 0x0405007821171295a716c9d091371e836e98a5206d5b9ce9177df90c83fc308ebae2786a9c7bff999ad83d12be08e597d4b5a5240f3bb0bc366f008b7d0908df8a 
+enode://efe233263c78482111ba6c058ccc69b7a2ea3372774733def4fd5a357dfbaa67657e665078d573f11876fd2b7d75d41926976f41e257f91b486e9d36f4143c8a@[::]:42562
+Whisper node started
+Connected to peer.
+Please enter the peer's public key:
+***************************************
+
+
+> wnode -standalone -ip=127.0.0.1:30379 -idfile=pk1.txt -fileexchange -savedir=/home/tester/tmp/msg
+
